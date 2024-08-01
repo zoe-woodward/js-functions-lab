d1cce94ad99c2744fc1c3cf9ac60747eb084e4f5
zoe-woodward <zoewoodward94@gmail.com>
2024-08-01 16:37:12 +0100

    adding extra exercises

--- app.js
+++ app.js
@@ -202,5 +202,76 @@ const basicCalculator = (x,y,operation) => {
 
 console.log('Exercise 9 Result:', basicCalculator(10, 5, "subtract"));
 
+// extra exercises
+ 
+/*
+Exercise 10: calculateGrade()
+
+Define a function called calculateGrade. 
+It should take a numerical score and return the corresponding letter 
+grade (A, B, C, D, F). 
+
+For example, 90 and above yields an 'A', 80-89 is a 'B', 
+and 70-79 is a 'C', 60-69 is a 'D' and anything lower than a 60 is an 'F'.
+
+Example: calculateGrade(100) should return A.
+
+Complete the exercise in the space below:
+*/
+
+const calculateGrade = (score) => {
+    if (score >= 90) {
+        return 'A';
+    } else if (score >= 80) {
+        return 'B';
+    } else if (score >= 70) {
+        return 'C';
+    } else if (score >= 60) {
+        return 'D';
+    } else {
+        return 'F';
+    }
+}
+console.log('Exercise 10 Result:', calculateGrade(75));
 
 
+/*
+Exercise 11: createUsername()
+
+Define a function called createUsername. 
+It should take a first name and a last name and return a username. 
+
+The username should be a combination of the following:
+- The first three letters of the first name.
+- The first three letters of the last name.
+- The total character count of the first and last name combined.
+
+Example: createUsername('Samantha', 'Green') should return 'SamGre13'.
+
+Complete the exercise in the space below:
+*/
+
+const createUsername = (firstName, lastName) => {
+    const firstPart = firstName.substring(0, 3);
+    const secondPart = lastName.substring(0, 3);
+    const totalLength = firstName.length + lastName.length;
+    return firstPart + secondPart + totalLength;
+}
+
+console.log('Exercise 11 Result:', createUsername('Samantha', 'Green'));
+
+/*
+Exercise 12: numArgs()
+
+Challenge yourself with numArgs. 
+This function should return the count of arguments passed to it when called.
+
+Complete the exercise in the space below:
+*/
+
+// using a rest parameter (...args) so the function can accept an indefinite number of arguments
+const numArgs = (...args) => {
+    return args.length;
+}
+
+console.log('Exercise 12 Result:', numArgs(1, 2, 3, 4));
